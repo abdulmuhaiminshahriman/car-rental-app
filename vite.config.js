0b88756c7d610c57e116208479fd5e475e8b1cad
muhaimin <muhaimin@aoneschools.com>
2023-09-18 06:22:58 +0800

    set port for local dev server and output dist location

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
   }
 })
