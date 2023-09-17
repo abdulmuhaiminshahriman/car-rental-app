4f965a0251f492326e8a29786ca58bafdf768eb5
muhaimin <muhaimin@aoneschools.com>
2023-09-18 06:24:43 +0800

    add routing

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar fixed-top bg-dark navbar-expand-lg bg-body-tertiary mb-r">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <div class="navbar-nav me-auto mb-2 mb-lg-0">
+          <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+          <RouterLink class="nav-link" aria-current="page" to="/login">Login</RouterLink>
+        </div>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style scoped>
+.nav-link.active {
+  color: red;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-6">
+    <span v-for="(item, index) in items" :key="index">
+      {{ item }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios';
+
+const items = ref([])
+
+onMounted(async () => {
+  const { data } = await axios.get('http://localhost:3004/posts');
+  items.value = data.data
+})
+</script>
+
+<style scoped>
+
+</style>
